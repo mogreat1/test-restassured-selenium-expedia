f15c8053415df71729d6dd74f58f13020172762e
Max <mogreat1@gmail.com>
2019-08-26 11:14:22 +0200

    converted testNG tests into Cucumber

--- src/test/java/features/Flights.feature
+++ src/test/java/features/Flights.feature
@@ -7,5 +7,47 @@ Feature: Flights
 
   Scenario: Typing the first letter into Fly From Filed user gets ten auto suggestions
     Given Flights Page is loaded
-    When user types first letter into Fly From field
+    When user types "k" into Fly From field
     Then amount of auto suggestions equals to ten
+
+  Scenario: Last auto suggestion for a Fly From field starts from the entered letter
+    Given Flights Page is loaded
+    When user types "S" into Fly From field
+    And choose last value from auto suggestion
+    And click Space button
+    Then value chosen filled into Fly From field starts from "S"
+
+  Scenario: After clicking Departing Data a Calendar is displayed
+    Given Flights Page is loaded
+    When user clicks Departing Data
+    Then Calendar is displayed
+
+  Scenario: After clicking Close button on the Calendar user Calendar disappears
+    Given Flights Page is loaded
+    When user clicks Departing Data
+    And clicks Close button
+    Then Calendar is not displayed
+
+  Scenario: Three groups are displayed in Traveler Dropdown
+    Given Flights Page is loaded
+    When user clicks Traveler Dropdown
+    Then Three Traveler groups are displayed
+
+  Scenario: After adding child Traveler in Traveler Dropdown child Age dropdown displayed
+    Given Flights Page is loaded
+    When user clicks Traveler Dropdown
+    And clicks Add child button
+    Then Child age button dropdown is displayed
+Scenario: Child Age dropdown contains all child ages
+  Given Flights Page is loaded
+  When user clicks Traveler Dropdown
+  And clicks Add child button
+  Then ChildAge Dropdown contains all child ages
+  Scenario: After loading Flights page Car checkbox is not selected
+    Given Flights Page is loaded
+    When User doesn't select anything
+    Then Add Car checkbox is not selected
+    Scenario: after checking Add Car checkbox Save Message is displayed
+      Given Flights Page is loaded
+      When check Add Car checkbox
+      Then Save Message is Displayed
--- src/test/java/stepDefinition/FlightsStepDefinition.java
+++ src/test/java/stepDefinition/FlightsStepDefinition.java
@@ -4,6 +4,7 @@ import base.BaseTest;
 import cucumber.api.PendingException;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -19,6 +20,7 @@ import java.util.Properties;
 
 public class FlightsStepDefinition extends BaseTest {
     private FlightsPage flightsPage;
+
     @Before
     public void setUp() {
         super.setUp();
@@ -41,9 +43,9 @@ public class FlightsStepDefinition extends BaseTest {
         Assert.assertTrue(flightsPage.isReturningDataInvisible());
     }
 
-    @When("^user types first letter into Fly From field$")
-    public void user_types_first_letter_into_fly_from_field() throws Throwable {
-        flightsPage.sendText("k").chooseLastAutoSugg().clickSpaceFlyingFrom();
+    @When("^user types \"([^\"]*)\" into Fly From field$")
+    public void user_types_something_into_fly_from_field(String strArg1) throws Throwable {
+        flightsPage.sendText(strArg1).chooseLastAutoSugg().clickSpaceFlyingFrom();
     }
 
     @Then("^amount of auto suggestions equals to ten$")
@@ -51,7 +53,88 @@ public class FlightsStepDefinition extends BaseTest {
         Assert.assertTrue(flightsPage.flyingFromFieldStarts("S"));
     }
 
+    @Then("^value chosen filled into Fly From field starts from \"([^\"]*)\"$")
+    public void value_chosen_filled_into_fly_from_field_starts_from_something(String strArg1) throws Throwable {
+        Assert.assertTrue(flightsPage.flyingFromFieldStarts("S"));
+    }
 
+    @And("^choose last value from auto suggestion$")
+    public void choose_last_value_from_auto_suggestion() throws Throwable {
+        flightsPage.chooseLastAutoSugg();
+
+    }
+
+    @And("^click Space button$")
+    public void click_space_button() throws Throwable {
+        flightsPage.clickSpaceFlyingFrom();
+    }
+
+    @When("^user clicks Departing Data$")
+    public void user_clicks_departing_data() throws Throwable {
+        flightsPage.clickDepartingData();
+
+    }
+
+    @Then("^Calendar is displayed$")
+    public void calendar_is_displayed() throws Throwable {
+        Assert.assertTrue(flightsPage.isCalendarDisplayed(1));
+    }
+
+    @Then("^Calendar is not displayed$")
+    public void calendar_is_not_displayed() throws Throwable {
+        Assert.assertTrue(flightsPage.isCalendarDisplayed(0));
+    }
+
+    @And("^clicks Close button$")
+    public void clicks_close_button() throws Throwable {
+        flightsPage.closeCalendar();
+    }
+
+    @When("^user clicks Traveler Dropdown$")
+    public void user_clicks_traveler_dropdown() throws Throwable {
+        flightsPage.clickTravelersDD();
+    }
+
+    @Then("^Three Traveler groups are displayed$")
+    public void three_traveler_groups_are_displayed() throws Throwable {
+        Assert.assertTrue(flightsPage.areAllTravelersGroupsPresent());
+    }
+
+    @Then("^Child age button dropdown is displayed$")
+    public void child_age_button_dropdown_is_displayed() throws Throwable {
+        Assert.assertTrue(flightsPage.isChildAgeDDDisplayed());
+    }
+
+    @And("^clicks Add child button$")
+    public void clicks_add_child_button() throws Throwable {
+        flightsPage.clickAddChild();
+    }
+
+
+    @Then("^ChildAge Dropdown contains all child ages$")
+    public void childage_dropdown_contains_all_child_ages() throws Throwable {
+        Assert.assertTrue(flightsPage.isAllChildAgesDisplayed());
+    }
+
+    @When("^User doesn't select anything$")
+    public void user_doesnt_select_anything() throws Throwable {
+
+    }
+
+    @Then("^Add Car checkbox is not selected$")
+    public void add_car_checkbox_is_not_selected() throws Throwable {
+        Assert.assertFalse(flightsPage.isAddCarBoxSelected());
+    }
+
+    @When("^check Add Car checkbox$")
+    public void check_add_car_checkbox() throws Throwable {
+        flightsPage.clickAddCarBox();
+    }
+
+    @Then("^Save Message is Displayed$")
+    public void save_message_is_displayed() throws Throwable {
+        Assert.assertTrue(flightsPage.isSaveMessageDisplayed());
+    }
 
     @After
     public void tearDown() {
