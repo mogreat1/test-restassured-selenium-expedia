24a43a98927eeee44de4f6c3788f2748c092882a
Max <mogreat1@gmail.com>
2019-09-02 15:35:56 +0200

    asdf

--- src/test/java/tests/NewTests.java
+++ src/test/java/tests/NewTests.java
@@ -1,4 +1,16 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class NewTests {
+
+    @Test
+    public void test(){
+        System.out.println("It's test");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Not test but ....");
+    }
 }
