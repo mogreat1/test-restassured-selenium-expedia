a312f1d6071d76081a579480a8f2ea5e1e78ac4f
mogreat1 <mogreat1@gmail.com>
2019-09-09 17:30:14 +0200

    home page

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,14 +1,17 @@
 package pages;
 
 import base.BasePage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
 
 public class HomePage extends BasePage {
 
@@ -23,18 +26,61 @@ public class HomePage extends BasePage {
     private List<WebElement> weekendGetawaysLinks;
     @FindBy(xpath = "//*[@id='cupid-popdestinations-title']")
     private WebElement popularDestinationTitle;
+    @FindBy(xpath = "//*[@class='utility-nav nav-group cf']//a")
+    private List<WebElement> topLinks;
 
 
-    public void  weekendGetawaysLinks() throws InterruptedException {
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        Thread.sleep(500);
-        jse.executeScript("window.scrollBy(0,1000)");
-        Thread.sleep(500);
-       List<WebElement> list =  driver.findElements(By.xpath("//*[@id='cupid-weekendgetaways-container']//a[@class='flex-link']"));
-        System.out.println(list.size());
-       // weekendGetawaysLinks.stream().forEach(element -> System.out.println(element));
+    public void openTopLinks() {
+
+        List<WebElement> topLinkList = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.
+                xpath("//*[@class='utility-nav nav-group cf']//a"), 5));
+        int k = 0;
+        for (int i = 0; i < topLinkList.size(); i++) {
+
+            String openLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
+            if (k < 5) {
+                topLinkList.get(i).sendKeys(openLink);
+                System.out.println(k);
+                k++;
+            } else {
+                System.out.println("break");
+                break;
+            }
+
+        }
+        Set<String> windows = driver.getWindowHandles();
+        Iterator iterator = windows.iterator();
+        String baseWindow = driver.getWindowHandle();
+
+        for (String window : windows) {
+            if (!window.equals(baseWindow)) {
+                driver.switchTo().window(window);
+                System.out.println(driver.getTitle());
+            }
+
+        }
+        driver.switchTo().defaultContent();
+        System.out.println(driver.getTitle());
+
     }
 
+    public void sendRequests() throws InterruptedException {
+        List<WebElement> topLinkList = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.
+                xpath("//*[@class='utility-nav nav-group cf']//a"), 5));
+        int k = 0;
+        for (WebElement element : topLinkList) {
+            if (k < 5) {
+                Thread.sleep(500);
+                int status = given().get(element.getAttribute("href")).then().extract().statusCode();
+                System.out.println(status);
+                k++;
+            } else {
+                break;
+            }
+
+
+        }
+    }
 
 
 }
--- src/test/java/tests/HomeTests.java
+++ src/test/java/tests/HomeTests.java
@@ -1,23 +1,32 @@
 package tests;
 
 import base.BaseTest;
+import io.restassured.RestAssured;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
+
+import static io.restassured.RestAssured.given;
 
 public class HomeTests extends BaseTest {
     private HomePage homePage;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         super.setUp();
         driver.get(prop.getProperty("baseUrl"));
         homePage = new HomePage(driver);
     }
 
     @Test
-    private void weekendGetawaysLinksValidTest() throws InterruptedException {
-    homePage.weekendGetawaysLinks();
+    private void weekendGetawaysLinksValidTest(){
+        homePage.openTopLinks();
+    }
+
+    @Test
+    private void sendRequestForLinksTest() throws InterruptedException {
+        homePage.sendRequests();
+
     }
 
 
