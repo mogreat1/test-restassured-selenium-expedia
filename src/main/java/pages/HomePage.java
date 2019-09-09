3341b1ed032dd978aa16684c405353b810c2c9c2
Max <mogreat1@gmail.com>
2019-09-09 14:19:48 +0200

    added home tests

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import base.BasePage;
+import com.aventstack.extentreports.gherkin.model.Given;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +9,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static io.restassured.RestAssured.given;
 
 public class HomePage extends BasePage {
 
@@ -19,22 +25,68 @@ public class HomePage extends BasePage {
     }
 
 
-    @FindBy(xpath = "//*[@id='cupid-weekendgetaways-container']//a[@class='flex-link']")
-    private List<WebElement> weekendGetawaysLinks;
-    @FindBy(xpath = "//*[@id='cupid-popdestinations-title']")
-    private WebElement popularDestinationTitle;
+    @FindBy(xpath = "//*[contains(@id,'primary-header')]")
+    private List<WebElement> headerLinks;
+    @FindBy(xpath = "//*[@id='package-departing-hp-package']")
+    private WebElement departingDate;
+    @FindBy(xpath = "//*[@class='datepicker-cal-date disabled']")
+    private List<WebElement> disabledDates;
+    @FindBy(xpath = "//*[@class='datepicker-cal-date']")
+    private List<WebElement> futureDates;
 
 
-    public void  weekendGetawaysLinks() throws InterruptedException {
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        Thread.sleep(500);
-        jse.executeScript("window.scrollBy(0,1000)");
-        Thread.sleep(500);
-       List<WebElement> list =  driver.findElements(By.xpath("//*[@id='cupid-weekendgetaways-container']//a[@class='flex-link']"));
-        System.out.println(list.size());
-       // weekendGetawaysLinks.stream().forEach(element -> System.out.println(element));
+    public int validateHeaderLinks() {
+        int k = 0;
+        Set<Integer> set = new HashSet<Integer>();
+        for (WebElement link : headerLinks) {
+            String url = link.getAttribute("href");
+
+            int code = given().get(url).then().extract().response().statusCode();
+            set.add(code);
+            k++;
+            if (k > 5) {
+                break;
+            }
+        }
+        return set.size();
+
     }
 
+    public int validateLink() {
+        int code = given().get("https://www.google.com").then().extract().response().statusCode();
+        return code;
+
+    }
+
+    public HomePage clickDepartingDate() {
+        clickElement(departingDate);
+        return this;
+    }
+
+    public boolean isAmountDisabledDaysLess() {
+        LocalDateTime now = LocalDateTime.now();
+        int dayOfMonth = now.getDayOfMonth();
+        int disabledDays = disabledDates.size();
+        return disabledDays < dayOfMonth;
+    }
+
+    public boolean isPastDayEnabled() {
+        WebElement pastDay = disabledDates.get(0);
+        return pastDay.isEnabled();
+    }
+
+    public boolean isCurrentDayEnabled() {
+        return futureDates.get(0).isEnabled();
+    }
+
+    public boolean isCurrentDay() {
+        LocalDateTime now = LocalDateTime.now();
+        int dayOfMonth = now.getDayOfMonth();
+        String date = futureDates.get(0).getText().strip();
+        System.out.println(date);
+        String day = Integer.toString(dayOfMonth);
+        return date.contains(day);
+    }
 
 
 }
--- src/test/java/tests/HomeTests.java
+++ src/test/java/tests/HomeTests.java
@@ -1,24 +1,56 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.HomePage;
 
 public class HomeTests extends BaseTest {
     private HomePage homePage;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         super.setUp();
         driver.get(prop.getProperty("baseUrl"));
         homePage = new HomePage(driver);
     }
 
     @Test
-    private void weekendGetawaysLinksValidTest() throws InterruptedException {
-    homePage.weekendGetawaysLinks();
+    private void weekendGetawaysLinksValidTest() {
+        int size = homePage.validateHeaderLinks();
+        Assert.assertEquals(size, 1);
     }
 
+    @Test
+    private void urlTest() {
+        int code = homePage.validateLink();
+        Assert.assertEquals(code, 200);
+    }
+
+    @Test
+    private void pastDaysDisabledTest() {
+        homePage.clickDepartingDate();
+        Assert.assertTrue(homePage.isAmountDisabledDaysLess());
+    }
+
+    @Test
+    private void pastDayEnabledTest() {
+        homePage.clickDepartingDate();
+        Assert.assertFalse(homePage.isPastDayEnabled());
+    }
+
+    @Test
+    private void currentDayEnabledTest() {
+        homePage.clickDepartingDate();
+        Assert.assertTrue(homePage.isCurrentDayEnabled());
+    }
+
+    @Test
+    private void currentDayFirstEnabledDayTest(){
+        homePage.clickDepartingDate();
+        Assert.assertTrue(homePage.isCurrentDay());
+    }
 
 }
